354baf6e4c48a5a5b61e39c392dc9d98b0759456
Jiri Rojicek <jiri@rojicek.cz>
2023-01-21 23:04:20 +0100

    init query

---
+++ php-hdo/hdo-query.php
@@ -0,0 +1,8 @@
+<?php
+// php ktere vrati info kdy je HO pro zpracovani v ESP
+// navratove hodnoty json nebo plain text - podle toho co se bude snaze parsovat v ESP
+
+require_once "includedb.php";
+
+
+?>
