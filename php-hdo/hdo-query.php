f0d7d58f075c41be5a8126af19b992bdf0fad2b1
Jiri Rojicek <jiri@rojicek.cz>
2023-01-22 11:53:09 +0100

    init tests

--- php-hdo/hdo-query.php
+++ php-hdo/hdo-query.php
@@ -5,4 +5,23 @@
 require_once "includedb.php";
 
 
+$intervals_obj = new stdClass();
+
+
+$intervals_obj->intervals = [["00:30", "01:30"], ["02:30", "33:33"]];
+
+
+$export_json = json_encode($intervals_obj);
+
+echo $export_json;
+echo "<p>";
+
+//var_dump(json_decode($export_json));
+//echo "<p>";
+
+//$a = $export_json->{'intervals'};
+//echo $a;
+
+// demo parsing json
+
 ?>
