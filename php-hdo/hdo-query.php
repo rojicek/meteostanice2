b78ecec4a2104fcf5b4b22504611ad862b6c63b7
Jiri Rojicek <jiri@rojicek.cz>
2023-01-22 19:16:29 +0100

    HOL support

--- php-hdo/hdo-query.php
+++ php-hdo/hdo-query.php
@@ -5,23 +5,38 @@
 require_once "includedb.php";
 
 
-$DoWdnes = " ucase(SUBSTRING(DAYNAME(now()),1,3)) ";  #mozna nahradim HOL, takze takhle
-$DoWzitra = " ucase(SUBSTRING(DAYNAME(DATE_ADD(now(), INTERVAL 1 DAY) ),1,3)) ";  #mozna nahradim HOL, takze takh
+$datetime_now = time();
 
-//todo: kontrola jestli nejaky den neni svatek !!!
+$date_today = date('Y-m-d', $datetime_now);
+$date_tomorrow = date('Y-m-d', $datetime_now + 86400);
+
+
+$holiday_today_sql = "select count(*) as pocet from holidays_tbl where datum = '" . $date_today . "'";
+$holiday_today_q = $databaseConnection -> query($holiday_today_sql);
+$row = $holiday_today_q->fetch_row();
+$holiday_today = (int)$row[0];
+if ($holiday_today == 1)
+    $DoWdnes = " 'HOL' ";
+else
+    $DoWdnes = " ucase(SUBSTRING(DAYNAME(now()),1,3)) ";  
+
+
+$holiday_tomorrow_sql = "select count(*) as pocet from holidays_tbl where datum = '" . $date_tomorrow . "'";
+$holiday_tomorrow_q = $databaseConnection -> query($holiday_tomorrow_sql);
+$row = $holiday_tomorrow_q->fetch_row();
+$holiday_tomorrow = (int)$row[0];
+if ($holiday_tomorrow == 1)
+    $DoWzitra = " 'HOL' ";
+else
+    $DoWzitra = " ucase(SUBSTRING(DAYNAME(DATE_ADD(now(), INTERVAL 1 DAY) ),1,3)) ";  
+
 
 $selectDNESKA = "select DoW, peakStart, peakStop from hdo_tbl where date(now())>=dateStart AND date(now())<=dateEnd AND " . $DoWdnes . "  = DoW order by timeid";  
-$hdoResultsDNESKA  = $databaseConnection->query ($selectDNESKA) ;
+$hdoResultsDNESKA  = $databaseConnection->query ($selectDNESKA);
    
 $selectZITRA = "select DoW, peakStart, peakStop from hdo_tbl where date(now())>=dateStart AND date(now())<=dateEnd AND " . $DoWzitra . "  = DoW order by timeid";  
-$hdoResultsZITRA  = $databaseConnection->query ($selectZITRA) ;
+$hdoResultsZITRA  = $databaseConnection->query ($selectZITRA);
 
- 
-$date_today = date('Y-m-d', time());
-$date_tomorrow = date('Y-m-d', time()+86400);
-
-$datetime_now = time();
-$datetime_str_now = date('Y-m-d H:i:s', $datetime_now);
 
   
 $intervals_obj = new stdClass();
@@ -38,7 +53,7 @@ $arr_data = array();
      }        
      elseif (strtotime($konec) >= $datetime_now)
      { 
-        array_push($arr_data, array($datetime_str_now, $konec));
+        array_push($arr_data, array(date('Y-m-d H:i:s', $datetime_now), $konec));
      }
   }
  
