52443c6f45bf7376cddaa83fd0e4935261832e56
Jiri Rojicek <jiri@rojicek.cz>
2023-01-25 18:13:20 +0100

    wip

--- sandbox/ino1/api_test_1/api_test_1.ino
+++ sandbox/ino1/api_test_1/api_test_1.ino
@@ -10,19 +10,11 @@
 
 ESP8266WiFiMulti WiFiMulti;
 
-#define HTDU21D_ADDRESS 0x40  //Unshifted 7-bit I2C address for the sensor
-
-#define TRIGGER_TEMP_MEASURE_HOLD 0xE3
-#define TRIGGER_HUMD_MEASURE_HOLD 0xE5
-#define TRIGGER_TEMP_MEASURE_NOHOLD 0xF3
-#define TRIGGER_HUMD_MEASURE_NOHOLD 0xF5
-#define WRITE_USER_REG 0xE6
-#define READ_USER_REG 0xE7
-#define SOFT_RESET 0xFE
-
-byte sensorStatus;
-
 StaticJsonDocument<768> doc;
+
+const char *  lat = "50.0973722";
+const char *  lon = "14.4074581";
+const char * API_key = "5cb11160e6649574aca2ee031feda8d4";
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,6 +24,7 @@ void setup() {
   WiFiMulti.addAP("R_host", "badenka5");
 
   Serial.println("inicializace hotova");
+  delay(1000);
 }
 
 void loop() {
@@ -63,7 +56,7 @@ void loop() {
           String payload = https.getString();
           Serial.println(payload);
 
-         
+
 
           DeserializationError error = deserializeJson(doc, payload);
 
@@ -75,43 +68,79 @@ void loop() {
 
           JsonArray intervals = doc["intervals"];
 
-          const char* intervals_0_0 = intervals[0][0];  // "2023-01-24 22:20:00"
-          const char* intervals_0_1 = intervals[0][1];  // "2023-01-24 23:00:00"
+          Serial.println("NACTENA DATA");
 
-          const char* intervals_1_0 = intervals[1][0];  // "2023-01-25 02:40:00"
-          const char* intervals_1_1 = intervals[1][1];  // "2023-01-25 03:20:00"
+          for (int i = 0; i < intervals.size(); i++) {
+            const char* cas_zacatek = intervals[i][0];
+            const char* cas_konec = intervals[i][1];
+            Serial.printf("zacatek: %s\n", cas_zacatek);
+            Serial.printf("konec: %s\n", cas_konec);
+          }
 
-          const char* intervals_2_0 = intervals[2][0];  // "2023-01-25 07:20:00"
-          const char* intervals_2_1 = intervals[2][1];  // "2023-01-25 08:00:00"
 
-          const char* intervals_3_0 = intervals[3][0];  // "2023-01-25 11:20:00"
-          const char* intervals_3_1 = intervals[3][1];  // "2023-01-25 12:00:00"
-
-          const char* intervals_4_0 = intervals[4][0];  // "2023-01-25 15:00:00"
-          const char* intervals_4_1 = intervals[4][1];  // "2023-01-25 15:40:00"
-
-          const char* intervals_5_0 = intervals[5][0];  // "2023-01-25 19:20:00"
-          const char* intervals_5_1 = intervals[5][1];  // "2023-01-25 20:00:00"
-
-          const char* intervals_6_0 = intervals[6][0];  // "2023-01-25 22:20:00"
-          const char* intervals_6_1 = intervals[6][1];  // "2023-01-25 23:00:00"
-
-          Serial.println ("NACTENA DATA");
-          Serial.printf("posledni: %s\n", intervals_6_1);
 
           ///////////////////
-          //fuguje          
+          //fuguje
         }
       } else {
         Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
       }
 
       https.end();
+
+
+
     } else {
       Serial.printf("[HTTPS] Unable to connect\n");
     }
-  }
 
-  delay(500000);
-  Serial.println("ping");
+    //pocasi (test)
+    char url_weather[250] = "https://api.openweathermap.org/data/3.0/onecall?lat=";
+    strcat(url_weather, lat);
+    strcat(url_weather, "&lon=");
+    strcat(url_weather, lon);
+    strcat(url_weather, "&appid=");
+    strcat(url_weather, API_key);
+    strcat(url_weather, "&units=metric");
+
+    Serial.printf("w url: %s\n", url_weather);
+
+      if (https.begin(*client, url_weather)) {
+      // HTTPS
+      Serial.print("[HTTPS] GET...\n");
+
+      // start connection and send HTTP header
+      int httpCode = https.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String w_payload = "--init--";
+           w_payload = https.getString();
+          Serial.println("payload pocasi:");
+          Serial.println(w_payload);
+          Serial.println("^^^");
+          /////////////////
+          //fuguje
+        }
+      } else {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+
+      https.end();
+
+
+
+    } else {
+      Serial.printf("[HTTPS] Unable to connect\n");
+    }
+
+
+  } //konec wifi
+
+  delay(30000);
 }
