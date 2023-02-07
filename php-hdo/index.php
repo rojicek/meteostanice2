a0036163e815a1d8e4ef4082bb42e921a772da15
Jiri Rojicek <jiri@rojicek.cz>
2023-02-07 20:14:46 +0100

    wip - funguje bez hdo

--- php-hdo/index.php
+++ php-hdo/index.php
@@ -30,7 +30,11 @@ $meteo_content_arr = json_decode($meteo_content, true);
 <table>
 
 <tr>
-<td class="maly" style="text-align:right;" colspan=4>
+<td rowspan=3>
+<?php echo "<img src=\"img/weather/". $meteo_content_arr["weather"]["w_icon"] . ".svg\"  style=\"vertical-align:middle\" width=200>"; ?>
+</td>
+
+<td class="maly" style="text-align:right;" colspan=3>
 <?php
  $current_time = time();
  echo date('j M, H:i', $current_time);
@@ -40,19 +44,17 @@ $meteo_content_arr = json_decode($meteo_content, true);
 
 
 <tr>
-<td rowspan=2>
-<img src="img/weather/weather_icon_demo.png">
-</td>
 
-<td colspan=3 class="maly" style="text-align:right;vertical-align: middle;">
+
+<td colspan=3 class="maly" style="text-align:right;vertical-align: middle;" height=70>
 <?php 
 echo date('H:i', $meteo_content_arr["weather"]["sunrise"]);
 echo "&nbsp;&nbsp;";
 
 if (($current_time < $meteo_content_arr["weather"]["sunrise"]) or ($current_time > $meteo_content_arr["weather"]["sunset"])) 
-    echo "<img src=\"img/sun/sunrise.png\" width=30>";
+    echo "<img src=\"img/sun/sunrise.svg\" style=\"vertical-align:middle\" width=80>";
 else
-    echo "<img src=\"img/sun/sunset.png\" width=30>";
+    echo "<img src=\"img/sun/sunset.svg\" style=\"vertical-align:middle\" width=80>";
 
 echo "&nbsp;&nbsp;"; 
 echo date('H:i',$meteo_content_arr["weather"]["sunset"]);
@@ -64,13 +66,19 @@ echo date('H:i',$meteo_content_arr["weather"]["sunset"]);
 <tr>
 
 <td>
-<img src="img/cycle/cycle_ok.png"  width=50>
+<font class="mensi">
+<?php echo strtoupper(date('D')) . "&nbsp;&nbsp;<img src=\"img/cycle/cycle_". $meteo_content_arr["weather"]["clc_tdy"] . ".png\"  style=\"vertical-align:middle\" width=70>"; ?>
+</font>
 </td>
+
 <td>
-<img src="img/cycle/cycle_ok.png"  width=50>
+<font class="mensi">
+<?php echo strtoupper(date("D", strtotime("+1 day"))) . "&nbsp;&nbsp;<img src=\"img/cycle/cycle_". $meteo_content_arr["weather"]["clc_tmr"] . ".png\"  style=\"vertical-align:middle\" width=70>"; ?>
+</font>
 </td>
+
 <td>
-<img src="img/air_quality/air.png"  width=50>
+<img src="img/air_quality/air<?php echo $meteo_content_arr["weather"]["aqi"]; ?>.png"  width=70>
 </td>
 
 </tr>
@@ -84,13 +92,14 @@ echo date('H:i',$meteo_content_arr["weather"]["sunset"]);
 </font>
 </td>
 <td colspan=3 rowspan=3 style="color:#355E3b">
-&nbsp;+1h +2h <br>&nbsp;+4h +11h 
+&nbsp;+0h +0h <br>&nbsp;+0h +0h 
 </td>
 </tr>
 
 
 <tr>
 <td>
+
 <img src="img/temp_trend/<?php echo $meteo_content_arr["weather"]["temp_trend_icon"]; ?>.png"  style="vertical-align:middle" width=40> 
 <font class="mensi"><?php echo $meteo_content_arr["weather"]["temp_trend"] . "&degC";  ?></font>
 </td>
@@ -99,12 +108,16 @@ echo date('H:i',$meteo_content_arr["weather"]["sunset"]);
 
 <tr>
 <td>
-<img src="img/direction/compass-ne.png" width=25 heigth=25> 
+<img src="img/direction/compass-<?php echo strtolower ($meteo_content_arr["weather"]["wind_dir"]); ?>.png"  style="vertical-align:middle" width=30 height=30> 
 <font style="font-size: smaller;"><?php echo $meteo_content_arr["weather"]["wind_speed"] . "m/s";?></font>
 </td>
 
 </tr>
 
+<tr>
+<td colspan=4  height=5px>
+</td>
+</tr>
 
 <tr>
 <td class="maly" style="text-align:right;" colspan=4 bgcolor="#00316E" height=25px>
