9c376848e88bd2d35efae501f773d21384a024d8
Jiri Rojicek <jiri@rojicek.cz>
2023-02-10 21:56:42 +0100

    aqi added

--- php-hdo/aqi.php
+++ php-hdo/aqi.php
@@ -31,21 +31,83 @@ if ($air_content)
    $pm10 = $air_quality_arr['list'][0]['components']['pm10'];
    $nh3 = $air_quality_arr['list'][0]['components']['nh3'];
    
-   echo "aqi: " .$aqi . "<br>";
-   echo "co: " .$co . "<br>";       
-   echo "no: " .$no . "<br>";
-   echo "no2: " .$no2 . "<br>";
-   echo "o3: " .$o3 . "<br>";
-   echo "pm2_5: " .$pm2_5 . "<br>";
-   echo "pm10: " .$pm10 . "<br>";
-   echo "nh3: " .$nh3 . "<br>";
+   $co_i = match_range($co, $aqi_limits["co"]);
+   $no_i = match_range($no, $aqi_limits["no"]);
+   $no2_i = match_range($no2, $aqi_limits["no2"]);
+   $o3_i = match_range($o3, $aqi_limits["o3"]);
+   $pm2_5_i = match_range($pm2_5, $aqi_limits["pm2_5"]);
+   $pm10_i = match_range($pm10, $aqi_limits["pm10"]);
+   $nh3_i = match_range($nh3, $aqi_limits["nh3"]);
    
-           
+   /*
+   echo "aqi: " .$aqi . "<br>";
+   echo "co: " . $co . " - " . $co_i . "<br>";       
+   echo "no: " .$no . " - " . $no_i . "<br>";   
+   echo "no2: " .$no2 . " - " . $no2_i . "<br>";    
+   echo "o3: " .$o3 . " - " . $o3_i . "<br>";    
+   echo "pm2_5: " .$pm2_5 . " - " . $pm2_5_i . "<br>";    
+   echo "pm10: " .$pm10 . " - " . $pm10_i . "<br>";   
+   echo "nh3: " .$nh3 . " - " . $nh3_i . "<br>";    
+     */       
  } //air ok
 
  
     
- echo "<p>konec";
+ //echo "<p>konec";
  
    
 ?>
+
+<body>
+<table>
+
+<tr>
+<td> Air </td>
+<td><img src= "img/air_quality/air<?php echo $aqi;?>.png" width=70></td>
+<td><?php echo $aqi;?></td>
+</tr>
+
+<tr>
+<td> CO </td>
+<td><img src= "img/air_quality/air<?php echo $co_i;?>.png" width=70></td>
+<td><?php echo $co;?></td>
+</tr>
+
+<tr>
+<td> NO </td>
+<td><img src= "img/air_quality/air<?php echo $no_i;?>.png" width=70></td>
+<td><?php echo $no;?></td>
+</tr>
+
+<tr>
+<td> NO2 </td>
+<td><img src= "img/air_quality/air<?php echo $no2_i;?>.png" width=70></td>
+<td><?php echo $no2;?></td>
+</tr>
+
+<tr>
+<td> O3 </td>
+<td><img src= "img/air_quality/air<?php echo $o3_i;?>.png" width=70></td>
+<td><?php echo $o3;?></td>
+</tr>
+
+<tr>
+<td> PM 2.5 </td>
+<td><img src= "img/air_quality/air<?php echo $pm2_5_i;?>.png" width=70></td>
+<td><?php echo $pm2_5;?></td>
+</tr>
+
+<tr>
+<td> PM 10 </td>
+<td><img src= "img/air_quality/air<?php echo $pm10_i;?>.png" width=70></td>
+<td><?php echo $pm10;?></td>
+</tr>
+
+<tr>
+<td> NH3 </td>
+<td><img src= "img/air_quality/air<?php echo $nh3_i;?>.png" width=70></td>
+<td><?php echo $nh3;?></td>
+</tr>
+
+</table>
+</body>
