d94e0c17697dad85fb4272bd08dce0cc49712755
Jiri Rojicek <jiri@rojicek.cz>
2023-02-22 19:47:57 +0100

    daily chart wip

--- php-hdo/day.php
+++ php-hdo/day.php
@@ -1,54 +1,42 @@
 <html>
   <head>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 
+
+<!--Load the AJAX API--> 
+<script type="text/javascript"
+src="https://www.google.com/jsapi"></script> <script type="text/javascript" 
+src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+</script> 
+
+
+
+
+ <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script type="text/javascript">
     
-
+    // Load the Visualization API and the piechart package.
+     google.load('visualization', '1', {'packages':['corechart']});
     
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawVisualization);
-      
-function drawVisualization() {
-    // Some raw data (not necessarily accurate)
-    /*
-    var data = google.visualization.arrayToDataTable([
-        ['cas', 'dest', 'snih',  'teplota'],//, {type: 'number', role: 'annotation'}],
-        ['1h', 1, 1.5, 18],
-        ['2h', 2, 1, 20],
-        ['3h', 1, 3.5, 18],
-        ['4h', 1, 1, 18],
-        ['5h', 2, 1.5, 20],
-        ['6h', 1, 3, 18],
-        ['7h', 1, 1, 18],
-        ['8h', 2, 1, 20],
-        ['9h', 0, 0, 18],
-        ['10h', 0, 0, 18],
-        ['11h', 1, 1, 18],
-        ['12h', 2, 1, 20],
-        ['13h', 1, 0, 18],
-        ['14h', 1, 0, 18],
-        ['15h', 2, 1, 20],
-        ['16h', 1, 0, 17],
-        ['17h', 0, 1, 16],
-        ['18h', 0, 1, 15],
-        ['19h', 0, 3.5, 14],
-        ['20h', 1, 4, 10],
-        ['21h', 1, 5.5, 9],
-        ['22h', 2, 5, 9],
-        ['23h', 1, 3, 10],
-       
-    ]);
-    */
+     
+     
+     
+    google.setOnLoadCallback(drawChartTVStatusInterval);
+    function drawChartTVStatusInterval() 
+    {
     
-    var data = $.ajax({
+      var jsonDataTVStatusInterval = $.ajax({
           url: "get-daily.php",
-          dataType: "json",
+          dataType:"json",
           async: false
           }).responseText;
-
-    var options = {
+      
+      
+      
+       var hourly_data = new google.visualization.DataTable(jsonDataTVStatusInterval);
+    
+      
+     var options = {
         isStacked: true,
         title : 'Predpoved',
 
@@ -70,12 +58,17 @@ function drawVisualization() {
        
     };
 
-    var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
-}
+  
       
-      
-    </script>
+        var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+        chart.draw(hourly_data, options);
+
+    
+    } 
+
+
+   </script>
+
   </head>
   <body>
     <div id="chart_div" style="width: 900px; height: 500px;"></div>
--- php-hdo/get-daily.php
+++ php-hdo/get-daily.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 $myArr = array(
   'cas' => array('1h', '2h','3h', '4h'),
   'dest' => array(1,1,1,1),
@@ -10,6 +11,44 @@ $myArr = array(
 $myJSON = json_encode($myArr);
 
 echo $myJSON;
+*/
+
+     
+    $table = array();
+        
+
+     
+    $table['cols'] = array(
+    array('type' => 'string', 'label' => 'cas'),
+    array('type' => 'number', 'label' => 'dest'), 
+    array('type' => 'number', 'label' => 'snih'),
+    array('type' => 'number', 'label' => 'teplota')      
+    );
+
+    $temp1 = array(
+    array('v' => '2002'),
+    array('v' => 1),
+    array('v' => 2),
+    array('v' => 3)  
+    );
+    
+    $temp2 = array(
+    array('v' => '2003'),
+    array('v' => 4),
+    array('v' => 5),
+    array('v' => 6)  
+    );
+
+    $rows[] = array('c' => $temp1);   
+    $rows[] = array('c' => $temp2);                                              
+                                         
+    
+    $table['rows'] = $rows;
+
+
+     $jsonTable = json_encode($table);
+  
+    echo $jsonTable;
 
 
 ?>
